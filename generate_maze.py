8963ca9bf66e1146df4c83bbaa60389af8be940f
SidSeed <37780037+SidSeed@users.noreply.github.com>
2020-04-26 00:04:52 +0200

    Update generate_maze.py

--- generate_maze.py
+++ generate_maze.py
@@ -3,9 +3,22 @@ import maze
 
 # Create maze using Pre-Order DFS maze creation algorithm
 def create_dfs(m):
-    # TODO: Implement create_dfs
-    pass
-
+   backtrack=[]
+   random_cell = random.randint(1,m.total_cells-1)
+   visited = [0] * m.total_cells
+   visited[random_cell] = 1
+   while(visited.count(1) < m.total_cells ):
+       cells_neighbor=m.cell_neighbors(random_cell)
+       if(len(cells_neighbor)>=1):
+            random_neighbor=cells_neighbor[random.randint(0,len(cells_neighbor)-1)]
+            m.connect_cells(random_cell,random_neighbor[0],random_neighbor[1])
+            backtrack.append(random_cell)
+            random_cell = random_neighbor[0]
+            visited[random_cell]=1
+       else:
+        random_cell=backtrack.pop()
+       m.refresh_maze_view()
+   m.state='solve'
 
 def main():
     current_maze = maze.Maze('create')
